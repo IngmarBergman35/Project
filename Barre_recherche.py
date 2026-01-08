30cf353832d08898d6273facd64fd0c53b990f98
IngmarBergman35 <rcrivell@immacjp2.fr>
2026-01-08 12:10:15 +0100

    Update Barre_recherche.py

--- Barre_recherche.py
+++ Barre_recherche.py
@@ -4,26 +4,33 @@ from fonction import *
 '''parametrage app'''
 app=tkinter.Tk() 
 app.resizable(False,False) #empecher l'app d'etre redimentionner en L et l
-app.config(bg="green") 
+app.config(bg="#002366") 
 frame1=tkinter.Frame(app,width=700,height=250) 
 frame1.grid(row=0,column=0,padx=10,pady=10) #pas oublier bordure pad
-frame1.config(bg='green')
+frame1.config(bg='#002366')
 
 '''zone de texte'''
-lab1=tkinter.Label(frame1,text='Moteur de recherche',font=('Cambria',20),bg='pink',fg='white')
+lab1=tkinter.Label(frame1,text='Moteur de recherche',font=('Cambria',20),bg='#FFC1CC',fg='black')
 lab1.grid(row=0,column=0,padx=5,pady=5)
-'''zone de saisie texte'''
-entry1=tkinter.Entry(frame1,bg='green',width=80)
+'''zone de saisie texte'''                                                   
+entry1=tkinter.Entry(frame1,bg='#FFC1CC',width=80)
 entry1.grid(row=1,column=0,padx=5,pady=5)
-'''fonction associée au bouton''
+'''fonction associée au bouton'''
 
 
 #bouton recherche
-button1=tkinter.Button(frame1,text='Rechercher',bg='purple',fg='white',command=recherche) 
+
+button1=tkinter.Button(frame1,text='Rechercher',bg='#FFC1CC',fg='black',command=recherche) 
 button1.grid(row=2,column=0,padx=5,pady=5) 
 
 ''''boucle infini pour pas de fin''' 
 
 app.mainloop()
     
+
+#B5F5D1
+#F4B5F5
+#C4FFE0
+#D6F5B5
+
     
